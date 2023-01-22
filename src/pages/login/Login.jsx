f184c3193e5db24f4307d84f30327edf71d631d1
Mochammad Yusuf <107377365+TehPuccupHarum@users.noreply.github.com>
2023-01-23 04:12:43 +0700

    Add files via upload

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,27 @@
+import "./login.scss"
+
+
+export const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password"/>
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
+export default Login
